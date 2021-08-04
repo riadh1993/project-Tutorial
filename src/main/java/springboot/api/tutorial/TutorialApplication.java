63ca5bfb9e8855c7fd600e9a3d25d83344d6b32f
riadh1993 <riadhkar1993@gmail.com>
2021-08-04 15:39:41 +0100

    fix shit

---
+++ src/main/java/springboot/api/tutorial/TutorialApplication.java
@@ -0,0 +1,13 @@
+package springboot.api.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TutorialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialApplication.class, args);
+    }
+}
